b44f3d7a4214c331c8c164f753b0c3f0ec9f027a
jcpark9 <jcpark@stanford.edu>
2014-05-19 23:53:30 -0700

    signin and signup

---
+++ Basic Tasks Prototype/signup.php
@@ -0,0 +1,26 @@
+<?php
+    $con=mysqli_connect("ggreiner.com","ggreiner_g","volley3","ggreiner_247");
+    // Check connection
+    if (mysqli_connect_errno()) {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+	$name = mysqli_real_escape_string($con, $_POST['name']);
+	$username = mysqli_real_escape_string($con, $_POST['username']);
+	$phone = mysqli_real_escape_string($con, $_POST['phonenum']);
+	$password = mysqli_real_escape_string($con, $_POST['password']);
+
+	$result = mysqli_query($con,"INSERT INTO Users (Name, Username, Phone, Password) VALUES('$name', '$username', '$phone', '$password')");
+
+	$result = mysqli_query($con,"SELECT UserId FROM Users WHERE Username = '$username'");
+	$row = mysqli_fetch_array($result);
+	$UserId = $row["UserId"];
+
+	echo $UserId;
+
+	session_start(); 
+	$_SESSION['UserId'] = $UserId;
+
+	header("Location: login.php");
+
+?>
