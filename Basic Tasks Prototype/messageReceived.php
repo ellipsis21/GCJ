221a5e0ac39b22a1594abb816184e4136c62da7d
Chiara Brosco <chbrosco@stanford.edu>
2014-05-21 20:06:53 -0700

    added notifications for leader
    question creators should be notified when they receive their first
    response, half of their responses and all of their responses. Bug- if a
    person responds more than once or sends a long response that means
    multiple texts, we will eventually falsely repot having all
    responces/half the responses

--- Basic Tasks Prototype/messageReceived.php
+++ Basic Tasks Prototype/messageReceived.php
@@ -1,4 +1,5 @@
 <?php
+    require "twilio-php-latest/Services/Twilio.php";
     session_start(); 
     $con=mysqli_connect("ggreiner.com","ggreiner_g","volley3","ggreiner_247");
     // Check connection
@@ -18,10 +19,40 @@
         $result = mysqli_query($con,"SELECT * FROM Questions WHERE GroupId=$GroupId AND Open=1");
         if($column = mysqli_fetch_array($result)) {
             $qId= $column['QuestionId'];
+            $questioner= $column['UserId'];
             if(!mysqli_query($con,"INSERT INTO Responses (QuestionID, Phone, Response) VALUES ('$qId', '$number', '$message')")) echo "failure! " . mysqli_error($con);
-        }
+            $result2 = mysqli_query($con,"SELECT * FROM Users WHERE UserId='$questioner'");
+            if($anotherRow = mysqli_fetch_array($result2)){
+                $originalSenderNumber= "+1".$row['Phone'];
+                $result3 = mysqli_query($con,"SELECT * FROM Members WHERE GroupId=$GroupId");
+                $numberExpectedResp= mysql_num_rows($result3); 
+                if($numberExpectedResp){
+                    $result4 = mysqli_query($con,"SELECT * FROM Responses WHERE QuestionID=$qId");
+                    $numResp= mysql_num_rows($result4); 
+                    if($numResp){
+                        $body= "Your question got its first responce!";
+                        $sendMes= false;
+                        if($numResp == 1) $sendMes= true;
+                        if($numResp == ($numberExpectedResp/2)){
+                            $body= "You have received half of your results"
+                            $sendMes= true;
+                        }
+                        if($numResp == $numberExpectedResp){
+                            $sendMes= true;
+                            $body= "All of your results are in!"
+                        }
+                        if($sendMes){
+                            $account_sid = 'ACe45cbecec1c4d969f362becc4dae5ce1'; 
+                            $auth_token = '2920745a17eb488e173b7ffe2310f958'; 
+                            $client = new Services_Twilio($account_sid, $auth_token); 
+                            $sms = $client->account->messages->sendMessage("+17542108538", $originalSenderNumber, $body);
+                        }
+                    }
+                }
+            }
 
-    }
+        }
+    }    
 
 
     header("content-type: text/xml");
