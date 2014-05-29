d3a3b77c7f316ca1092124331e1b4084fb88f4ce
jcpark9 <jcpark@stanford.edu>
2014-05-29 01:31:29 -0700

    OR

--- Basic Tasks Prototype/manage.php
+++ Basic Tasks Prototype/manage.php
@@ -71,7 +71,7 @@
 			<a class="question-1" onclick="document.getElementById('myform').submit();"><?php echo $button ?></a>
 		</form>
 		<?php if (!isset($_GET['share'])) { ?>
-		<div class='heading'>Share Access to Signup</div>
+		<div class='heading'>Or Share Access to Signup</div>
 		<div class='subheading'>Send the following link to your members <br/> for them to add themselves to the group.</div>
 		<input id="share" class="textbox" type="text" value="<?php echo $curUrl ?>" onFocus="this.selectionStart=0; this.selectionEnd=this.value.length;" onTouchEnd="this.selectionStart=0; this.selectionEnd=this.value.length;" onMouseUp="return false"/>
 
